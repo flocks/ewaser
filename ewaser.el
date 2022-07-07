1ddcb58f48d5b467b50fc93f316529aedb7cdbd7
Florent Teissier <teissierflorent@gmail.com>
2022-07-07 18:17:24 +0200

    init README and add (provide) macro

--- ewaser.el
+++ ewaser.el
@@ -1,21 +1,21 @@
-(defun eww-cleaner--get-class-name (str)
+(defun ewaser--get-class-name (str)
   "Return the css classname from STR"
   (and (s-starts-with-p "." str)
 	   (cadr (split-string str "\\."))))
 
-(defun eww-cleaner--get-id-name (str)
+(defun ewaser--get-id-name (str)
   "Return the css id from STR"
   (and (s-starts-with-p "#" str)
 	   (cadr (split-string str "\\#"))))
 
-(defun eww-cleaner--get-node-by-selector (dom selector)
+(defun ewaser--get-node-by-selector (dom selector)
   "Giving DOM and a css SELECTOR, return the nodes"
-  (if-let ((class-name (eww-cleaner--get-class-name selector)))
+  (if-let ((class-name (ewaser--get-class-name selector)))
 	  (dom-by-class dom class-name)
-	(if-let ((id-name (eww-cleaner--get-id-name selector)))
+	(if-let ((id-name (ewaser--get-id-name selector)))
 		(dom-by-id dom id-name))))
 
-(defun eww-cleaner-clean (selectors)
+(defun ewaser-clean (selectors)
   "Take a list of css selector in SELECTORS (separated by space)
 and remove the associated nodes from the DOM in the current eww
 buffer."
@@ -23,11 +23,11 @@ buffer."
   (unless (eq major-mode 'eww-mode)
 	(user-error "Not in a EWW buffer"))
   (let* ((data eww-data)
-		 (dom (eww-cleaner--get-dom))
+		 (dom (ewaser--get-dom))
 		 (base (plist-get data :url))
 		 (selectors (split-string selectors)))
 	(dolist (s selectors)
-	  (dolist (node (eww-cleaner--get-node-by-selector dom s))
+	  (dolist (node (ewaser--get-node-by-selector dom s))
 		(dom-remove-node dom node)))
 	(eww-display-html nil nil
 					  (list 'base (list (cons 'href base))
@@ -37,7 +37,7 @@ buffer."
 	  (plist-put eww-data elem (plist-get data elem)))
 	(eww--after-page-change)))
 
-(defun eww-cleaner--get-dom ()
+(defun ewaser--get-dom ()
   "Retrieve the DOM object from the current eww buffer. Extracted
 from eww code."
   (let ((data eww-data))
@@ -50,34 +50,23 @@ from eww code."
 	  (libxml-parse-html-region (point-min) (point-max)))))
 
 
-(setq eww-cleaner-rules-alist '(("\\**stackoverflow\\|stackexchange*" .
-								 ("#left-sidebar"
-								  "#sidebar"
-								  "#footer"
-								  ".s-topbar--container"
-								  ".post-taglist"
-								  ".answers-subheader"
-								  ".js-post-menu"
-								  ".votecell"
-))))
 
-(defun eww-cleaner--hook ()
+(defun ewaser--hook ()
   "Hook passed to eww-after-render-hook"
   (let* ((url (plist-get eww-data :url))
-		 (rules (eww-cleaner--get-rules url eww-cleaner-rules-alist)))
+		 (rules (ewaser--get-rules url ewaser-rules-alist)))
 	(when rules
-	  (eww-cleaner-clean (string-join rules " ")))))
+	  (ewaser-clean (string-join rules " ")))))
 
 
-(defun eww-cleaner--get-rules (url alist)
+(defun ewaser--get-rules (url alist)
   (catch 'match
 	(dolist (entry alist)
 	  (let ((key (car entry)))
 		(when (string-match-p key url)
 		  (throw 'match (cdr entry)))))))
 
-(add-hook 'eww-after-render-hook 'eww-cleaner--hook)
-(remove-hook 'eww-after-render-hook 'eww-cleaner--hook)
+(provide 'ewaser)
 
 
 
